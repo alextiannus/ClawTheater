3b9bd5e85949e4212c07fe1afc303ac1fb2ed32b
alextiannus <alextiannus@gmail.com>
2026-04-03 18:23:06 +0800

    chore: cleanup test scripts

--- frontend/user_query.ts
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const user = await prisma.user.findFirst({ where: { email: "cs@clawtheater.com" } });
-  console.log("USER:", user);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
